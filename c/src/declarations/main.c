e13b896bea2d18088e46e0baa901cbf9ddbeff93
SMOL <smolnero@gmail.com>
2026-09-10 12:16:03 -0600

    declare

--- c/src/declarations/main.c
+++ c/src/declarations/main.c
@@ -3,20 +3,20 @@
 int main(void){
 
     /*OG variabes*/ 
-    float profit;
-    int height = 8;
+    int height = 0;
     int id = 27833;
     
    /*changing the values of vars*/  
-    profit = 3.99; 
     height = 20;
     
-    float humanSum = profit * height;
-    float const myKey = humanSum; 
+    float humanSum = id * height;
+    float const profit = humanSum; 
     
     /*printing an int*/
     printf("ID number: %d\n", id);
     
     /*printing an float - displays 4 digit after decimal*/
-    printf("Your perm key is: %.4f\n", myKey); 
+    printf("Your profit is: %.2f\n", profit);
+
+    printf("Both height: %d & id: %d\n", height, id);
 }
