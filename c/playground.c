fa4b86fd647bc383a441db391c3a847b5cef54e4
SMOL <smolnero@gmail.com>
2026-09-10 14:15:47 -0600

    scanf

--- c/playground.c
+++ c/playground.c
@@ -1,21 +1,9 @@
-/* Computes the dimensional weight of a 12" x 10" x 8" box */
 #include<stdio.h>
 
 int main(void){
-    int height, length, width, volume, weight;
+    int x;
 
-    printf("Enter height of box: ");
-    scanf("%d", &height);
-    printf("Enter length of box: ");
-    scanf("%d", &length);
-    printf("Enter width of box: "); 
-    scanf("%d", &width);
-
-    volume = height * length * width;
-    weight = (volume + 165) / 166; 
-
-    printf("Total Volume(Cubic inches): %d\n", volume);
-    printf("Total Weight (Pounds): %d\n", weight);
-
-  return 0;
+    printf("Think of a number and I will guess it: ");
+    scanf("%d", &x);
+    printf("Is your number %d?\n", x);
 }
