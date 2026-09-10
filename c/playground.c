efbcdda4803216bc5353a55afcc99f4d8a7e6faa
SMOL <smolnero@gmail.com>
2026-09-10 11:27:22 -0600

    cleanup

---
+++ c/playground.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void){
+
+    /*OG variabes*/ 
+    float profit;
+    int height = 8;
+    int id = 27833;
+    
+   /*changing the values of vars*/  
+    profit = 3.99; 
+    height = 20;
+
+    float humanSum = profit * height;
+    float const myKey = humanSum; 
+    
+    /*printing an int*/
+    printf("ID number: %d\n", id);
+    /*printing an float*/
+    printf("Your perm key is: %.2f\n", myKey); 
+}
