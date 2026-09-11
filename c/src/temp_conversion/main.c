523b3cc352f10fe4cdfa31d5f8b42f22df6b2473
SMOL <smolnero@gmail.com>
2026-09-11 10:05:12 -0600

    celc

---
+++ c/src/temp_conversion/main.c
@@ -0,0 +1,19 @@
+/*Converts from faranheight to celsius*/
+#include<stdio.h>
+
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void)
+{
+    float farenheit, celsius; 
+
+    printf("Please ender farenheit temperature: ");
+      scanf("%f", &farenheit); 
+
+    celsius = (farenheit - FREEZING_PT) * SCALE_FACTOR;
+
+    printf("Cesius equivalant: %.2f\n", celsius);
+    
+    return 0;
+}
