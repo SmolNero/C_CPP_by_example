e7ec441d7f0014f8209feb65ae2ccfb65a748ef1
SMOL <smolnero@gmail.com>
2026-09-12 14:15:15 -0600

    olo

--- c/src/bill_division/amount.c
+++ c/src/bill_division/amount.c
@@ -9,10 +9,24 @@ int main(void)
 
     int amount, twenties, tens, fives, ones;
 
-    printf("Enter the dollar amount.: ");
+    printf("Enter the dollar amount: ");
      scanf("%d", &amount);
     
     twenties = amount / 20; 
-    
+     amount = amount - (twenties * 20); 
 
+    tens = amount / 10;
+     amount = amount - (tens * 10);
+
+    fives = amount / 5;
+     amount = amount - (fives * 5);
+
+    ones = amount;
+    
+    printf("$20 bills: %d\n", twenties );
+    printf("$10 bills: %d\n", tens);
+    printf("$5 bills: %d\n", fives);
+    printf("$1 bills: %d\n", ones);
+
+    return 0;
 }
---
+++ c/src/hello_smort/arm64/main.s
@@ -0,0 +1,25 @@
+	.build_version macos, 26, 0	sdk_version 26, 5
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	stp	x29, x30, [sp, #-16]!           ; 16-byte Folded Spill
+	mov	x29, sp
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	adrp	x0, l_.str@PAGE
+	add	x0, x0, l_.str@PAGEOFF
+	bl	_printf
+	mov	w0, #0                          ; =0x0
+	ldp	x29, x30, [sp], #16             ; 16-byte Folded Reload
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"Hello, smort!\n"
+
+.subsections_via_symbols
