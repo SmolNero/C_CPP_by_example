66f754cb6ea41fa2a023fd9bffc3c2ce99f7dbf4
SMOL <smolnero@gmail.com>
2026-09-13 10:36:42 -0600

    olo

--- c/src/bill_division/amount.c
+++ c/src/bill_division/amount.c
@@ -1,25 +1,23 @@
 
 //Given a whole-dollar amount, use as many large bills as possible before moving to smaller bills.
 //integer division automatically throws away the fractional part.
-
 #include<stdio.h>
 
 int main(void)
 {
-
     int amount, twenties, tens, fives, ones;
 
     printf("Enter the dollar amount: ");
      scanf("%d", &amount);
     
     twenties = amount / 20; 
-     amount = amount - (twenties * 20); 
+    amount = amount - (twenties * 20); 
 
     tens = amount / 10;
-     amount = amount - (tens * 10);
+    amount = amount - (tens * 10);
 
     fives = amount / 5;
-     amount = amount - (fives * 5);
+    amount = amount - (fives * 5);
 
     ones = amount;
     
