5c77e017e4362bcb8b3224490b532c9646f86397
SMOL <smolnero@gmail.com>
2026-09-14 11:03:45 -0600

    loans

--- c/src/loan_bal/loan.c
+++ c/src/loan_bal/loan.c
@@ -2,6 +2,21 @@
 
 int main(void)
 {
+    float balance;
+    float annual_interest_rate;
+    float mounthly_interest_rate;
+    float payment;
+
+    printf("Please enter the total amount of your loan: ");
+    scanf("%..2f", &balance);
+
+    printf("Enter interest rate: ");
+    scanf("%..2f", &annual_interest_rate);
+
+    printf("What are your monthly payments: ");
+    scanf("%..2f", &payment);
+
+    mounthly_interest_rate = (annual_interest_rate / 100.0f) / 12.0f;
 
     return 0; 
 }
