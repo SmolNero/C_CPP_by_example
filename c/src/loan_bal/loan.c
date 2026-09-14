f19c96157ef0c65d01c56f16a9391cc8a242e3d4
SMOL <smolnero@gmail.com>
2026-09-14 12:05:29 -0600

    c

--- c/src/loan_bal/loan.c
+++ c/src/loan_bal/loan.c
@@ -8,15 +8,26 @@ int main(void)
     float payment;
 
     printf("Please enter the total amount of your loan: ");
-    scanf("%..2f", &balance);
+    scanf("%f", &balance);
 
     printf("Enter interest rate: ");
-    scanf("%..2f", &annual_interest_rate);
+    scanf("%f", &annual_interest_rate);
 
     printf("What are your monthly payments: ");
-    scanf("%..2f", &payment);
+    scanf("%f", &payment);
 
     mounthly_interest_rate = (annual_interest_rate / 100.0f) / 12.0f;
+    balance = balance + (balance * mounthly_interest_rate) - payment;
+
+    printf("Balance remaining after first payment: $%.2f\n", balance);
+
+    balance = balance + (balance * mounthly_interest_rate) - payment;
+
+    printf("Balance remaining after second payment: $%.2f\n", balance);
+   
+    balance = balance + (balance * mounthly_interest_rate) - payment;
+
+    printf("Balance remaining after third payment: $%.2f\n", balance);
 
     return 0; 
 }
